a0de794cbc08b1e567c32c3d96eb6878788d0170
Praveenb <p.boyalapalli@pccsuk.com>
2019-02-07 14:40:50 +0530

    applied messageview id to dialoglayout

--- library/src/main/java/com/yarolegovich/lovelydialog/AbsLovelyDialog.java
+++ library/src/main/java/com/yarolegovich/lovelydialog/AbsLovelyDialog.java
@@ -15,7 +15,6 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
